96ef3ee7861a72fa4cc4e7d168dc00c1fdd7993e
mazuh <marcell-mz@hotmail.com>
2021-01-12 09:36:28 -0300

    Update Sidekick dist

---
+++ sidekick/_next/static/D4lbaQ04QC3wums4zjVFL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fa2dae17b6e6b96acb753.css","static\u002Fchunks\u002Fpages\u002Findex-16d0e46ef1b9e29fc832.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
